20dd3e5712f43739d2cdc22856d262273c37b2ff
unknown <Karim984514@gmail.com>
2022-04-23 22:28:42 -0700

    'ktu'

--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -50,7 +50,7 @@ export default function GlobalStyles() {
         },
 
         //scroll bar
-        '.MuiPaper-root':{
+        '.css-1693cdt-MuiPaper-root':{
         scrollbarColor: "#6b6b6b #2b2b2b",
           "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
             backgroundColor: "rgb(238,238,238)",
